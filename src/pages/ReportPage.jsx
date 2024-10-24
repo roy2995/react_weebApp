affe9fe87b47e970c5ad52a5a6604a5c331f157e
Roderick <roypanatec@gmail.com>
2024-10-24 11:55:58 -0500

    solution of npm?

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/General/Header';
 import pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 
 // Función para convertir una imagen desde una URL a Base64
